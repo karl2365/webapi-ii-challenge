eee9a63ef34cbf97e205d990dea1d80a3d456229
karl2365 <karlbeksa@gmail.com>
2019-08-06 18:41:58 -0400

    MVP complete

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -133,7 +133,47 @@ router.delete('/:id', (req, res) => {
         });
 })
 
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
 
+    db.findById(id)
+        .then(post => {
+            if(post.length > 0){
+                console.log(changes.title, changes.contents)
+                if(changes.title && changes.contents){
+                    db.update(id, changes)
+                        .then(updated => {
+                            console.log(id);
+                            db.findById(id)
+                                .then(updatedPost => {
+                                    
+                                    res.status(200).json(updatedPost)
+
+                                })
+                                .catch (err => {
+                                    res.status(500).json({message: "error modifying data"})
+                                })
+                        })
+                        .catch(err => {
+                            res.status(500).json({error: "The post information could not be modified."});
+                        })
+
+
+                }
+                else {
+                    res.status(400).json({errorMessage: "Please provide title and contents for the post"});
+                }
+
+            }
+            else {
+                res.status(404).json({message: "The post with that ID does not exist."});
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: "The post information could not be modified."});
+        })
+})
 
 
 
